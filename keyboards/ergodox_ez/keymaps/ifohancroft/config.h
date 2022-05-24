54fbb60d45c768a1a8e9295ab1933b90d64a3ea9
IFo Hancroft <contact@ifohancroft.com>
2022-05-24 03:47:56 +0300

    Added my ErgoDox keymap

--- keyboards/ergodox_ez/keymaps/ifohancroft/config.h
+++ keyboards/ergodox_ez/keymaps/ifohancroft/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2021 IFo Hancroft
+/* Copyright 2022 IFo Hancroft
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,5 +17,13 @@
 #pragma once
 
 // place overrides here
-#define USB_POLLING_INTERVAL_MS 1
 #define FORCE_NKRO
+#define USB_POLLING_INTERVAL_MS 1
+#ifdef TAPPING_TOGGLE
+    #undef TAPPING_TOGGLE
+#endif
+#define TAPPING_TOGGLE 3
+#define VIAL_KEYBOARD_UID {0x6F, 0xBC, 0x77, 0x48, 0xE1, 0x7B, 0xF1, 0x46}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 13 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
+
